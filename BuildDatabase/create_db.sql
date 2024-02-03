ba7bac196273d2b22faaccf34d538fecd4e79961
mdesafari <mydataengineeringjourney@gmail.com>
2024-02-03 10:32:26 -0500

    create countries table, create cities table

--- BuildDatabase/create_db.sql
+++ BuildDatabase/create_db.sql
@@ -14,3 +14,34 @@ CREATE TABLE currencies (
     currency_code VARCHAR(3)
 );
 
+-- 2. create countries table
+DROP TABLE IF EXISTS countries;
+CREATE TABLE countries (
+    country_id INT AUTO_INCREMENT PRIMARY KEY,
+    country_name VARCHAR(50) NOT NULL,
+    country_code VARCHAR(50),
+    region VARCHAR(50),
+    sub_region VARCHAR(100),
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    -- create our foreign key
+    currency_id INT,
+    FOREIGN KEY (currency_id) REFERENCES currencies(currency_id)
+);
+
+-- 3. create cities table
+DROP TABLE IF EXISTS cities;
+CREATE TABLE cities (
+    city_id INT AUTO_INCREMENT PRIMARY KEY,
+    city_name VARCHAR(50) NOT NULL,
+    latitude DECIMAL(8, 4),
+    longitude DECIMAL(8, 4),
+    capital VARCHAR(5),
+    population INT,
+    inserted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    -- create the foreign key
+    country_id INT,
+    FOREIGN KEY (country_id) REFERENCES countries(country_id)
+);
+
