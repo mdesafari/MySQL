952d585ffaf9b27044917497d55628397c42da37
mdesafari <mydataengineeringjourney@gmail.com>
2024-02-03 10:52:43 -0500

    drop tables and db

---
+++ CleanDB/clean_db.sql
@@ -0,0 +1,8 @@
+-- select our db
+USE country_db;
+
+-- drop tables and the db in this order
+DROP TABLE IF EXISTS cities;  -- because cities depends on countries
+DROP TABLE IF EXISTS countries;  -- because countries depends on currencies
+DROP TABLE IF EXISTS currencies;
+DROP DATABASE IF EXISTS country_db;
